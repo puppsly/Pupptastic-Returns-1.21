6482c316ec94f543de5db1d7226110c2bbc10575
Puppsly <mitziederren@gmail.com>
2024-10-02 09:19:25 -0700

    Rose Quartz Recipes Added

--- src/main/java/net/puppsly/pupptasticreturns/block/ModBlocks.java
+++ src/main/java/net/puppsly/pupptasticreturns/block/ModBlocks.java
@@ -3,6 +3,7 @@ package net.puppsly.pupptasticreturns.block;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -10,6 +11,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.puppsly.pupptasticreturns.PupptasticReturns;
 
 public class ModBlocks {
@@ -18,16 +20,19 @@ public class ModBlocks {
                     .requiresTool().sounds(BlockSoundGroup.STONE)));
 
     public static final Block ROSE_QUARTZ_ORE = registerBlock("rose_quartz_ore",
-            new Block(AbstractBlock.Settings.create().strength(3f)
-                    .requiresTool().sounds(BlockSoundGroup.STONE)));
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                    AbstractBlock.Settings.create().strength(3f)
+                            .requiresTool().sounds(BlockSoundGroup.STONE)));
 
     public static final Block DEEPSLATE_ROSE_QUARTZ_ORE = registerBlock("deepslate_rose_quartz_ore",
-            new Block(AbstractBlock.Settings.create().strength(3f)
-                    .requiresTool().sounds(BlockSoundGroup.STONE)));
+            new ExperienceDroppingBlock(UniformIntProvider.create(3, 6),
+                    AbstractBlock.Settings.create().strength(4f)
+                            .requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
 
     public static final Block NETHERRACK_ROSE_QUARTZ_ORE = registerBlock("netherrack_rose_quartz_ore",
-            new Block(AbstractBlock.Settings.create().strength(2f)
-                    .requiresTool().sounds(BlockSoundGroup.NETHERRACK)));
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                    AbstractBlock.Settings.create().strength(2f)
+                            .requiresTool().sounds(BlockSoundGroup.NETHERRACK)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
